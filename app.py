e6720d101ed1545ab0d2ab64aa47d408e1d0f9ef
vishwateja27 <39549751+vishwateja27@users.noreply.github.com>
2018-11-27 13:39:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	my_name="hi to :"+string(name)
+	my_name="hi tolgl :"+string(name)
 	return render_template('index.html', name=my_name)
 
 
